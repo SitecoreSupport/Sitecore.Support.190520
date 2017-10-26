2fef2f0135a7d2b1d964fabb3c555056cf7f651d
DK\AEC <aec@sitecore.net>
2017-10-26 08:58:21 +0300

    commit patch

---
+++ src/Sitecore.Support.190520/CompositeEdit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Shell.Framework.Commands.WebDAV;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Support.Shell.Framework.Commands.WebDAV
+{
+    public class CompositeEdit : Sitecore.Shell.Framework.Commands.WebDAV.CompositeEdit
+    {
+        public override void Execute(CommandContext context)
+        {
+            if (context.Items.Length != 1)
+            {
+                return;
+            }
+
+            Sitecore.Configuration.State.Client.UsesBrowserWindows = true;
+
+            Item item = context.Items[0];
+            Command editingCommand = this.GetEditingCommand(item);
+            if (editingCommand == null)
+            {
+                return;
+            }
+            editingCommand.Execute(context);
+        }
+    }
+}
